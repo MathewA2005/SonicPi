ebc2214a8e0a31670161ac9b05080f577071c89f
MathewA2005 <99358817+MathewA2005@users.noreply.github.com>
2022-02-16 10:57:10 -0800

    Update ChallengeC.rb

--- ChallengeC.rb
+++ ChallengeC.rb
@@ -1,1 +1,107 @@
+use_bpm 160
+use_synth :saw
+
+play :c2
+sleep 0.5
+play :e2
+sleep 0.5
+play :g2
+sleep 0.5
+play :b2
+sleep 0.5
+
+play :c3
+sleep 0.5
+play :b2
+sleep 0.5
+play :g2
+sleep 0.5
+play :e2
+sleep 0.5
+
+with_fx :ixi_techno do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+with_fx :panslicer do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+with_fx :vowel do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+with_fx :reverb do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+
+play :c2, sustain: 3
+play :e2, sustain: 4
+play :g2, sustain: 2
+play :b2, sustain: 2
 
